7de14a6ad04e0c42b0638ff0884a6098d924f0a5
sat2llite <raumschiff_@naver.com>
2023-05-04 10:38:09 +0900

    메뉴 수정

--- js/main.js
+++ js/main.js
@@ -13,16 +13,15 @@ window.onload = () => {
   section01.style.opacity = "1";
   musicIcon.style.width = "86px";
   musicIcon.style.height = "86px";
-  headline.style.transform = "translateY(30px)"
+  headline.style.transform = "translateY(30px)";
   headline.style.opacity = "1";
 };
-
 
 let scroll = 50;
 // devices 스크롤 시 움직임
 window.addEventListener("wheel", (event) => {
-  devicesItem.forEach((item)=>{
-    if (event.deltaY > 0) {   
+  devicesItem.forEach((item) => {
+    if (event.deltaY > 0) {
       if (scroll < 0) {
         item.style.transform = `translateY(0px)`;
         scroll = 0;
@@ -37,15 +36,14 @@ window.addEventListener("wheel", (event) => {
         item.style.transform = `translateY(${(scroll += 3)}px)`;
       }
     }
-  })
+  });
 });
-
 
 // 아코디언메뉴 (하다만거)
 for (let i = 0; i < arcodian.length; i++) {
   arcodian[i].addEventListener("click", () => {
     arcodianText.classList.toggle("on");
-  })
+  });
 }
 
 //progress bar
@@ -108,6 +106,82 @@ userBtn.addEventListener("click", function () {
 });
 
 
+
+//메인 비디오
+
+// 1575부터 스크롤 이벤트 발생해서
+// width:가95%로줄어들고 고정 되어서 비디오가 스크롤 되다가 3072쯤에서 끝난다
+
+
+
+
+
+
+
+
+
+// const aa = document.querySelector('.section2 video');
+
+// document.addEventListener('scroll', function(){
+//   if(scrollY >= 1575){
+//     aa.classList.add('active');
+//     aa.style.position="sticky";
+//     aa.style.borderRadius="20px";
+//   }else if(scrollY < 1575){
+//     aa.classList.remove('active');
+//   } else {
+//     return;
+//   }
+// });
+
+
+
+
+
+
+
+
+
+
+
+// y축 좌표 입력할 때 큰 숫자부터 위로오게 설정해야함 maybe
+// document.addEventListener('scroll', function(){
+//   if(scrollY > 3200){
+//     aa.classList.remove('active');
+//   }else if(scrollY > 1575){
+//     aa.classList.add('active');
+//     aa.style.position="sticky";
+//   } else if(scrollY < 1575){
+//     aa.classList.remove('active');
+//   } else {
+//     return;
+//   }
+// });
+
+
+
+
+
+
+// intersectionobserver 체신기술 
+const aa = document.querySelectorAll('.section2 > video');
+
+const video = new IntersectionObserver((bb)=>{
+  console.log(bb);
+  bb.forEach((cc)=>{
+    if(cc.isIntersecting){
+      cc.target.classList.add('active');
+    } else {
+      cc.target.classList.remove('active');
+    }
+  });
+}, { thredhold: 0.5} );
+
+aa.forEach((bb)=>{
+  video.observe(bb);
+})
+
+
 // 햄버거 메뉴
 const hamburgerMenu = document.querySelector(".m_header_ul");
 const hamburgerIcon = document.querySelector(".hamburger");
@@ -117,10 +191,12 @@ hamburgerIcon.addEventListener("click", () => {
   hamburgerIcon.classList.toggle("on");
 
   if (hamburgerIcon.classList.contains("on")) {
+    document.body.classList.add("hidden");
     hamburgerMenu.style.visibility = "visible";
     hamburgerMenu.style.opacity = "1";
     gnb.style.visibility = "hidden";
   } else {
+    document.body.classList.remove("hidden");
     hamburgerMenu.style.visibility = "hidden";
     hamburgerMenu.style.opacity = "0";
     gnb.style.visibility = "visible";
